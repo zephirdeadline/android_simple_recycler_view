efb17ee8e81bf9fdc40575ccc1f6a512c746ee37
W4pity <w4pity@gmail.com>
2018-03-26 03:02:52 +0200

    migrate on github

---
+++ app/src/main/java/net/w4pity/androidtest/MoviesAdapter.java
@@ -0,0 +1,63 @@
+package net.w4pity.androidtest;
+
+/**
+ * Created by w4pity on 20/11/17.
+ */
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MyViewHolder> {
+
+    private List<Movie> moviesList;
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        public TextView title, year, genre;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            genre = (TextView) view.findViewById(R.id.genre);
+            year = (TextView) view.findViewById(R.id.year);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.d("test clic", "onClick: " + title.getText());
+        }
+    }
+
+
+    public MoviesAdapter(List<Movie> moviesList) {
+        this.moviesList = moviesList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.movie_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Movie movie = moviesList.get(position);
+        holder.title.setText(movie.getTitle());
+        holder.genre.setText(movie.getGenre());
+        holder.year.setText(movie.getYear());
+    }
+
+    @Override
+    public int getItemCount() {
+        return moviesList.size();
+    }
+}
